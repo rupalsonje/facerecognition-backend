8126cf247d0f01ceedb1130b3a8be412fefea6ba
Rupal1183 <rupalsonje0107@gamil.com>
2020-07-19 23:36:52 +0530

    host changed

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-spherical-32180',
     user : 'postgres',
-    password : 'rupal',
+    password : '',
     database : 'smart_brain'
   }
 });
