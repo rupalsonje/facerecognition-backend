4ddeca7c21f1ea89bc85ae25ee23789fe6748409
Rupal1183 <rupalsonje0107@gamil.com>
2020-07-23 22:15:13 +0530

    changes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const pg = require('pg');
 
 
 const register = require('./controllers/register');
@@ -12,7 +11,7 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  client: pg,
+  client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
     ssl: {
